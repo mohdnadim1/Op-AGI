612e126537f23558b25a5db47ba09d1172c64233
nadim <121538043+mohdnadim1@users.noreply.github.com>
2025-01-30 15:45:48 +0600

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Welcome to Op-AGI</h1>
+      <p>Building the future of Artificial General Intelligence</p>
+    </div>
+  );
+}
+
+export default App;
